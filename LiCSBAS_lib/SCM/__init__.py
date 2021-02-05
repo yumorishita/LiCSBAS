b722bb69e3608e2d3974134ec1d323f3fb06c630
Yu Morishita <asobu.morimori@gmail.com>
2021-02-05 15:15:08 +0900

    Update ScientificColourMaps to ver7

--- LiCSBAS_lib/SCM/__init__.py
+++ LiCSBAS_lib/SCM/__init__.py
@@ -6,9 +6,10 @@ from matplotlib.colors import LinearSegmentedColormap
 folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 
 __all__ = {
- 'acton', 'bamako', 'batlow', 'berlin', 'bilbao', 'broc', 'brocO', 'buda', 'cork', 'corkO', 'davos', 'devon', 'grayC', 'hawaii', 'imola', 'lajolla', 'lapaz', 'lisbon', 'nuuk', 'oleron', 'oslo', 'roma', 'romaO', 'tofino', 'tokyo', 'turku', 'vik', 'vikO'}
+ 'acton', 'bam', 'bamO', 'bamako', 'batlow', 'batlowK', 'batlowW', 'berlin', 'bilbao', 'broc', 'brocO', 'buda', 'bukavu', 'cork', 'corkO', 'davos', 'devon', 'fes', 'grayC', 'hawaii', 'imola', 'lajolla', 'lapaz', 'lisbon', 'nuuk', 'oleron', 'oslo', 'roma', 'romaO', 'tofino', 'tokyo', 'turku', 'vanimo', 'vik', 'vikO'}
 
 for name in __all__:
     file = os.path.join(folder, name, name + '.txt')
     cm_data = np.loadtxt(file)
     vars()[name] = LinearSegmentedColormap.from_list(name, cm_data)
+    vars()[name + '_r'] = LinearSegmentedColormap.from_list(name + '_r', np.flip(cm_data, axis=0))
--- LiCSBAS_lib/extract_cmaps_SCM.sh
+++ LiCSBAS_lib/extract_cmaps_SCM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-indir="ScientificColourMaps6"
+indir="ScientificColourMaps7"
 outdir="SCM"
 init="$outdir/__init__.py"
 
@@ -40,6 +40,7 @@ for name in __all__:
     file = os.path.join(folder, name, name + '.txt')
     cm_data = np.loadtxt(file)
     vars()[name] = LinearSegmentedColormap.from_list(name, cm_data)
+    vars()[name + '_r'] = LinearSegmentedColormap.from_list(name + '_r', np.flip(cm_data, axis=0))
 EOF
 
 
